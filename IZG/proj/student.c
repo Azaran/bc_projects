3f60bbe8c47037f94529ea8b3ec262192ef74917
Vojtěch Večeřa <xvecer18@stud.fit.vutbr.cz>
2015-04-10 12:11:24 +0200

    [EDIT] Vykresleni 2 objektu

---
+++ IZG/proj/student.c
@@ -0,0 +1,211 @@
+/******************************************************************************
+ * Projekt - Zaklady pocitacove grafiky - IZG
+ * spanel@fit.vutbr.cz
+ *
+ * $Id: student.c 416 2015-03-10 08:07:46Z spanel $
+ */
+
+#include "student.h"
+#include "transform.h"
+#include "fragment.h"
+
+#include <memory.h>
+#include <math.h>
+
+
+/*****************************************************************************
+ * Globalni promenne a konstanty
+ */
+
+/* Typ/ID rendereru (nemenit) */
+const int           STUDENT_RENDERER = 1;
+
+/* Castecne pruhledny material */
+const S_Material    MAT_STUD_AMBIENT  = { 0.2, 0.8, 0.2, 0.4 };
+const S_Material    MAT_STUD_DIFFUSE  = { 0.2, 0.8, 0.2, 0.4 };
+const S_Material    MAT_STUD_SPECULAR = { 0.8, 0.8, 0.8, 1.0 };
+
+
+/*****************************************************************************
+ * Funkce vytvori vas renderer a nainicializuje jej
+ */
+
+S_Renderer * studrenCreate()
+{
+    S_StudentRenderer * renderer = (S_StudentRenderer *)malloc(sizeof(S_StudentRenderer));
+    IZG_CHECK(renderer, "Cannot allocate enough memory");
+
+    /* inicializace default rendereru */
+    renderer->base.type = STUDENT_RENDERER;
+    renInit(&renderer->base);
+
+    /* nastaveni ukazatelu na upravene funkce */
+    /* napr. renderer->base.releaseFunc = studrenRelease; */
+    /* ??? */
+
+    /* inicializace nove pridanych casti */
+    /* ??? */
+
+    return (S_Renderer *)renderer;
+}
+
+/*****************************************************************************
+ * Funkce korektne zrusi renderer a uvolni pamet
+ */
+
+void studrenRelease(S_Renderer **ppRenderer)
+{
+    S_StudentRenderer * renderer;
+
+    if( ppRenderer && *ppRenderer )
+    {
+        /* ukazatel na studentsky renderer */
+        renderer = (S_StudentRenderer *)(*ppRenderer);
+
+        /* pripadne uvolneni pameti */
+        /* ??? */
+        
+        /* fce default rendereru */
+        renRelease(ppRenderer);
+    }
+}
+
+/*****************************************************************************
+ * Funkce inicializuje buffery ve vasem rendereru
+ */
+
+void studrenCreateBuffers(S_Renderer *pRenderer, int width, int height)
+{
+    S_StudentRenderer   * renderer;
+
+    IZG_ASSERT(pRenderer && width > 0 && height > 0);
+
+    /* ukazatel na studentsky renderer */
+    renderer = (S_StudentRenderer *)pRenderer;
+
+    /* alokace pameti pro buffery a vymazani obsahu bufferu */
+    /* ??? */
+
+    /* zavolame take puvodni funkci */
+    renCreateBuffers(pRenderer, width, height);
+}
+
+/*****************************************************************************
+ * Funkce vycisti buffery ve vasem rendereru pred kreslenim noveho framu
+ */
+
+void studrenClearBuffers(S_Renderer *pRenderer)
+{
+    S_StudentRenderer   * renderer;
+
+    IZG_ASSERT(pRenderer);
+
+    /* ukazatel na studentsky renderer */
+    renderer = (S_StudentRenderer *)pRenderer;
+
+    /* vymazeme a inicializujeme buffery */
+    /* ??? */
+
+    /* zavolame take puvodni funkci */
+    renClearBuffers(pRenderer);
+}
+
+/******************************************************************************
+ * Nova fce pro rasterizaci trojuhelniku s podporou vykreslovani s pruhlednosti
+ * v1, v2, v3 - ukazatele na vrcholy trojuhelniku ve 3D pred projekci
+ * n1, n2, n3 - ukazatele na normaly ve vrcholech ve 3D pred projekci
+ * x1, y1, ... - vrcholy trojuhelniku po projekci do roviny obrazovky
+ */
+
+void studrenDrawTriangle(S_Renderer *pRenderer,
+                         S_Coords *v1, S_Coords *v2, S_Coords *v3,
+                         S_Coords *n1, S_Coords *n2, S_Coords *n3,
+                         int x1, int y1,
+                         int x2, int y2,
+                         int x3, int y3
+                         )
+{
+    /* zaklad fce zkopirujte z render.c */
+}
+
+/******************************************************************************
+ * Vykresli i-ty trojuhelnik modelu pomoci nove fce studrenDrawTriangle()
+ * Pred vykreslenim aplikuje na vrcholy a normaly trojuhelniku
+ * aktualne nastavene transformacni matice!
+ * i - index trojuhelniku
+ */
+
+void studrenProjectTriangle(S_Renderer *pRenderer, S_Model *pModel, int i)
+{
+    /* zaklad fce zkopirujte z render.c */
+    
+}
+
+/******************************************************************************
+ ******************************************************************************
+ * Funkce pro vyrenderovani sceny, tj. vykresleni modelu
+ * Upravte tak, aby se model vykreslil s pruhlednosti a materialem
+ */
+
+void renderStudentScene(S_Renderer *pRenderer, S_Model *pModel)
+{
+    /* zaklad fce zkopirujte z main.c */
+    /* test existence frame bufferu a modelu */
+    IZG_ASSERT(pModel && pRenderer);
+
+    /* nastavit projekcni matici */
+    trProjectionPerspective(pRenderer->camera_dist, pRenderer->frame_w, pRenderer->frame_h);
+
+    /* vycistit model matici */
+    trLoadIdentity();
+
+    /* nejprve nastavime posuv cele sceny od/ke kamere */
+    trTranslate(0.0, 0.0, pRenderer->scene_move_z);
+    
+
+    /* nejprve nastavime posuv cele sceny v rovine XY */
+    trTranslate(pRenderer->scene_move_x, pRenderer->scene_move_y, 0.0);
+
+    /* natoceni cele sceny - jen ve dvou smerech - mys je jen 2D... :( */
+    trRotateX(pRenderer->scene_rot_x);
+    trRotateY(pRenderer->scene_rot_y);
+    
+    S_Matrix init;
+    trGetMatrix(&init); // ulozeni nastaveni sceny
+    
+   
+    
+    // prvni objekt
+
+    /* nastavime material */
+    renMatAmbient(pRenderer, &MAT_RED_AMBIENT);
+    renMatDiffuse(pRenderer, &MAT_RED_DIFFUSE);
+    renMatSpecular(pRenderer, &MAT_RED_SPECULAR);
+
+    // pridame posun
+    trTranslate(-1.0,0.0,0.0);
+
+    /* a vykreslime nas model */
+    renderModel(pRenderer, pModel);
+    
+  
+    
+    // navrat do puvodniho stavu po nastaveni sceny
+    trSetMatrix(&init);
+    
+    // druhy objekt
+    
+    /* nastavime material */
+    renMatAmbient(pRenderer, &MAT_STUD_AMBIENT);
+    renMatDiffuse(pRenderer, &MAT_STUD_DIFFUSE);
+    renMatSpecular(pRenderer, &MAT_STUD_SPECULAR);
+    
+    // pridame posun
+    trTranslate(1.0,0.0,0.0);
+    
+    renderModel(pRenderer, pModel);
+}
+
+
+/*****************************************************************************
+ *****************************************************************************/
